93dd8069fd6b6cac95c9b7f322cd154a3692d43b
Amitaukande <96390843+Amitaukande@users.noreply.github.com>
2022-02-23 21:38:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import "./styles.css";
+const heading = {
+  color: "red",
+  textTransform: "capitalize",
+  textAlign: "center"
+};
+export default function App() {
+  return (
+    <div className="App">
+      <h1 style={heading}>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
